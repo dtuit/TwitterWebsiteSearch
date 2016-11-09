c6205f2d0da422e468d77b0bdf168288f0dd9f5c
dtuit <darren.tuit+git@gmail.com>
2016-11-09 22:51:38 +1300

    refactor querys

--- TwitterWebsiteSearch/TwitterQuery.py
+++ TwitterWebsiteSearch/TwitterQuery.py
@@ -4,9 +4,7 @@ from datetime import datetime
 class SearchType():
     
     """
-    
     Enum for working SearchType parameters.
-    
     """
 
     Top = ''
@@ -14,29 +12,64 @@ class SearchType():
     Videos = 'Videos'
     News = 'news'
 
-class SearchQuery():
+class BaseQuery():
+    def __init__(self):
+       pass
+    
+    def build(self):
+        raise NotImplementedError
+
+class UserTimelineQuery(BaseQuery):
+    def __init__(self, screen_name, min_position=None, max_position=None, reset_error_state=False, next_page_query=True, **kwargs):
+        
+        self.base_url = 'https://twitter.com/i/search/timeline'
+        self.user = user
+        self.max_position = max_position
+        self.min_position = min_position
+        self.reset_error_state = reset_error_state
+        self.is_next_page_query = next_page_query
+
+    def build(self):
+        raise NotImplementedError
+
+
+class SearchQuery(BaseQuery):
 
     """
-    
-    Construct the url query string for a search query 
+    Bundle of parameters to Construct the url query string for a search query 
     
     :param query: the query string
-    :param min_tweetId: 
-        
+    :param max_position: used as a cursor to which next page is desired.
+    :param min_position: 
+    :param since:
+    :param until:
+    :param search_type: 
+    :param reset_error_state: When True query will always return the first page of results, result will include both the min_position and max_position parameters.
+    :param next_page_query: Determines if to send the max_postion(next page) parameter or min_position(new tweets)
+    :param **kwargs:
     """
 
-    def __init__(self, query, max_position=None, min_position=None, min_tweetId=None, max_tweetId=None, since=None, until=None, searchtype=SearchType.Top, latent_count=None):
+    def __init__(self,
+            query,
+            max_position=None,
+            min_position=None,
+            since=None,
+            until=None,
+            search_type=SearchType.Top,
+            reset_error_state=False,
+            next_page_query=True,
+            **kwargs #TODO 
+            ):
 
         self.base_url = 'https://twitter.com/i/search/timeline'
         self.query = query
         self.max_position = max_position
         self.min_position = min_position
-        self.min_tweetId = min_tweetId
-        self.max_tweetId = max_tweetId
         self.since = since
         self.until = until
-        self.searchType = searchtype
-        self.latent_count = latent_count
+        self.search_type = search_type
+        self.reset_error_state = reset_error_state
+        self.is_next_page_query = next_page_query
 
     def build(self):
         params = {}
@@ -48,17 +81,38 @@ class SearchQuery():
             q += ' until:' + self.until.strftime('%Y-%m-%d')
         params['q'] = quote(q)
 
-        params['max_position'] = ''
-        if self.min_tweetId is not None and self.max_tweetId is not None:
-            params['max_position'] = self.encode_max_postion_param(self.min_tweetId, self.max_tweetId)
+        if self.is_next_page_query:
+            params['max_position'] = '' if not self.max_position else self.max_position
+        else:
+            params['min_position'] = '' if not self.min_position else self.min_position
         
-        if self.searchType is not SearchType.Top:
+        if self.search_type is not SearchType.Top:
             params['f'] = self.searchType
+        
+        params['reset_error_state'] = self.reset_error_state
 
         query_str = "&".join("%s=%s" % (k,v) for k,v in params.items())
 
         return query_str
 
-    @staticmethod
-    def encode_max_postion_param(min_tweetId, max_tweetId):
-        return "TWEET-{0}-{1}".format(min_tweetId, max_tweetId)
+    def set_max_postion(self, min_tweetId, max_tweetId, extra=None):
+        mp = "TWEET-{0}-{1}".format(min_tweetId, max_tweetId)
+        if extra is not None:
+            mp += '-{0}'.format(extra)
+        self.max_position = mp
+    
+    def set_min_position(self, min_tweetId, max_tweetId, extra=None):
+        mp = "TWEET-{0}-{1}".format(max_tweetId, min_tweetId)
+        if extra is not None:
+            mp += '-{0}'.format(extra)
+        self.min_position = mp
+
+    def autoset_reset_error_state(self):
+        """
+        Set self.reset_error_state based on the combination of max_position, min_position and is_next_page_query
+
+        """
+        if self.is_next_page_query is True and self.max_position is None or self.is_next_page_query is False and self.min_position is None or self.min_position is None and self.max_position is None:
+            self.reset_error_state = True
+        else:
+            self.reset_error_state = False
