bb10f6dc1616342b6ac834902b41efd8d8823e65
dtuit <darren.tuit+git@gmail.com>
2016-12-06 20:56:34 +1300

    add example

--- examples.py
+++ examples.py
@@ -1,1 +1,10 @@
-#TODO add examples
+from TwitterWebsiteSearch import TwitterClient, SearchQuery
+
+client = TwitterClient()
+query = SearchQuery('#python')
+
+count = 0
+for page in client.get_search_iterator(query):
+    for tweet in page['tweets']:
+        print("{0} id: {1} text: {2}".format(count, tweet['id_str'], tweet['text']))
+        count += 1
